a78ac8128961722fcc26231ecb4b237f3f1ddbdf
TehreemAkhtar <tehreemakhtar33@gmail.com>
2024-12-16 22:38:59 +0500

    Convert product function views to class based views

--- store/views.py
+++ store/views.py
@@ -3,36 +3,40 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from store.models import Product, Collection
 from store.serializers import ProductSerializer, CollectionSerializer
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, product_id):
-    product = Product.objects.get(pk=product_id)
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self, request, product_id):
+        product = Product.objects.get(pk=product_id)
         serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'PUT':
+
+    def put(self, request, product_id):
+        product = Product.objects.get(pk=product_id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, product_id):
+        product = Product.objects.get(pk=product_id)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an orderitem'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
